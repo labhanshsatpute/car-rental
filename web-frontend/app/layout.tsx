8293532406658ea8dc217aa8b2e36d7662be807b
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2024-06-25 00:25:29 +0530

    bug fixes

--- web-frontend/app/layout.tsx
+++ web-frontend/app/layout.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import '../styles/globals.css';
-import { Inter } from 'next/font/google'
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import { combineReducers, legacy_createStore } from 'redux';
 import AuthReducer from "../redux/reducers/AuthReducer"
 import AppLayout from './app';
 const store = legacy_createStore(combineReducers({ AuthReducer }));
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -23,7 +20,7 @@ export default function RootLayout({
           <title>CarsHub</title>
           <link rel="icon" href="/favicon.ico" sizes="any" />
         </head>
-        <body className={inter.className}>
+        <body>
           <AppLayout children={children} />
         </body>
       </html>

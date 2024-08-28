837b0d0318a394b01e793e6c781cd7c797acd70d
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2024-08-29 00:40:36 +0530

    changes

--- web-frontend/app/dashboard/settings/page.tsx
+++ web-frontend/app/dashboard/settings/page.tsx
@@ -4,12 +4,14 @@ import { Breadcrumbs, CustomButton } from '@/components';
 import { AuthUserLogin } from '@/redux/actions/AuthAction';
 import { updateProfile } from '@/services/user';
 import Link from 'next/link';
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { IoCameraOutline } from "react-icons/io5";
 
 const Settings = () => {
 
   const [profileImage, setProfileImage] = useState(null);
+  const [profileImagePreview, setProfileImagePriview] = useState('https://innostudio.de/fileuploader/images/default-avatar.png');
 
   const dispatch = useDispatch();
   
@@ -23,13 +25,26 @@ const Settings = () => {
   const handleProfileImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       setProfileImage(event.target.files[0] as any);
+      setProfileImagePriview(URL.createObjectURL(event.target.files[0]));
     }
     else {
+      if (auth?.profileImage) {
+        setProfileImagePriview(`${process.env.NEXT_PUBLIC_STORAGE_BASE_URL}/${auth.profileImage}`);
+      }
+      else {
+        setProfileImagePriview('https://innostudio.de/fileuploader/images/default-avatar.png');
+      }
       setProfileImage(null);
     }
   }
 
   const auth = useSelector((state: any) => state.AuthReducer);
+
+  useEffect(() => {
+    if (auth?.profileImage) {
+      setProfileImagePriview(`${process.env.NEXT_PUBLIC_STORAGE_BASE_URL}/${auth.profileImage}`);
+    }
+  }, [auth]);
 
   if (!auth) {
     return "Loading...";
@@ -51,18 +66,21 @@ const Settings = () => {
           }
         ]} />
         <div className='container'>
-          <div className='lg:py-12 md:py-12 sm:py-5'>
+          <div className='lg:py-12 md:py-12 sm:py-10'>
 
 
             <div className='grid lg:col-span-3'>
               
-                <div className='flex lg:flex-row md:flex-row sm:flex-col lg:items-center md:items-center sm:items-start gap-4'>
-                  <div className='h-[90px] w-[90px] rounded-full overflow-clip border'>
-                    <img src={auth.profileImage ? `${process.env.NEXT_PUBLIC_STORAGE_BASE_URL}/${auth.profileImage}` : 'https://innostudio.de/fileuploader/images/default-avatar.png'} alt="" className='h-auto w-auto' />
+                <div className='flex lg:flex-row sm:flex-col items-center lg:justify-start sm:justify-center gap-5'>
+                  <div className='h-[100px] w-[100px] rounded-full overflow-clip relative group ring-4 ring-ascent-dark'>
+                    <input onChange={(event) => handleProfileImageChange(event)} type="file" name='profileImage' className='absolute h-full w-full cursor-pointer opacity-0 z-10' />
+                    <div className='h-full w-full bg-black bg-opacity-20 absolute bottom-0 hidden group-hover:flex items-center justify-center'>
+                      <IoCameraOutline size={25} className='stroke-white' />
+                    </div>
+                    <img src={profileImagePreview} alt="" className='h-full w-auto' />
                   </div>
                   <div className='space-y-[1px]'>
-                    <input onChange={(event) => handleProfileImageChange(event)} type="file" name='profileImage' />
-                    <CustomButton handleClick={() => handleUploadProfileImage()} text='Update Profile' />
+                    <button className='px-4 py-3 text-sm font-medium bg-gray-200 rounded-lg' onClick={() => handleUploadProfileImage()}>Upload Image</button>
                   </div>
                 </div>
 
